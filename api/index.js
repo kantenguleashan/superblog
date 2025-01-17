ac1c81bddc06d5c35fb2316e5d529e788d339da0
Ashankamtengule <kantenguleshan@gmail.com>
2025-01-17 13:16:29 +0200

    register page

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require('cors');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const app = express();
 
+const salt = bcrypt.genSaltSync(10);
 app.use(cors());
 app.use(express.json());
 
@@ -11,11 +13,13 @@ mongoose.connect('mongodb+srv://superblogger:ALCh7gbI5P04fq1m@cluster0.hjtdf.mon
 
 app.post('/register', async (req, res)=>{
 const {username,password} = req.body;   
-try{const userDoc= await User.create({username,password}); 
+try{const userDoc= await User.create({
+   username,
+   password:bcrypt.hashSync(password,salt), 
+}); 
 res.json(userDoc);
 }catch(e){
-   res.status(400).json(e);
-   
+res.status(400).json(e);
 }
 });
 
