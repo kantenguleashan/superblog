9213a10b0c291f436a078f2b1c69f811848d2878
Ashankamtengule <kantenguleshan@gmail.com>
2025-01-23 15:48:56 +0200

    finishing code

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,22 +1,8 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import {useState} from "react";
-import { Navigate } from 'react-router-dom';
-const  modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
-    ]
-  };
-  const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-  ];
+import { Navigate } from 'react-router-dom'; 
+ import Editor  from "../Editor";
 
 export default function CreatePost(){
     const [title,setTitle] = useState('');
@@ -62,10 +48,8 @@ return (
         onChange={ev => setSummary(ev.target.value)}/>
         <input type="file" 
         onChange={ev => setFiles(ev.target.files)}/>
-        <ReactQuill value={content}
-        onChange={newValue => setContent(newValue)}
-         modules={modules} 
-         formats={formats}/>
+        <Editor value ={content} onChange={setContent}/>
+      
         <button style={{marginTop: '5px'}}> Create Post</button>
     </form>
 
--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,57 +1,60 @@
-import { useState } from "react";
-import ReactQuill from 'react-quill';
-import {Navigate} from 'react-router-dom';
+import {useEffect, useState } from "react";
+import {Navigate,useParams} from 'react-router-dom';
+import Editor from "../Editor";
 
 export default function EditPost(){
-    
+    const {id} = useParams();
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content, setContent] = useState('');
     const [files, setFiles] = useState('');
     const [redirect,setRedirect] = useState('');
 
+    useEffect(()=> {
+        fetch('http://localhost:4000/post/'+ id)
+        .then(response => {response.json().then(postInfo => {
+            setTitle(postInfo.title);
+            setContent(postInfo.content);
+            setSummary(postInfo.summary); 
+        })
+    })
+    },[]);
+/**
+ * Handles the submission of the updated post form by preventing the default
+ * form submission, gathering form data, and sending an update request to the server.
+ * If the update is successful, it sets the redirect state to true to navigate
+ * back to the homepage.
+ * 
+ * @param {Event} ev - The event object for the form submission.
+ */
+
     function updatePost(ev){
         ev.preventDefault();
 
     }
-
 
     if(redirect){
      
         return <Navigate to={'/'}/>
      
        }
-       const  modules = {
-        toolbar: [
-          [{ 'header': [1, 2, false] }],
-          ['bold', 'italic', 'underline','strike', 'blockquote'],
-          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-          ['link', 'image'],
-          ['clean']
-        ]
-      };
-      
-
-     return (
+return (
          <form onSubmit={updatePost}>
-             <input type="title" 
+         <input type="title" 
              placeholder={'Title'} 
              value={title} 
              onChange={ev => 
              setTitle(ev.target.value)}/>
-             <input type="summary"
+         <input type="summary"
              placeholder={'Summary'}
              value ={summary}
              onChange={ev => setSummary(ev.target.value)}/>
-             <input type="file" 
-             onChange={ev => setFiles(ev.target.files)}/>
-             <ReactQuill value={content}
-             onChange={newValue => setContent(newValue)}
-              modules={modules}/>
-             <button style={{marginTop: '5px'}}> Edit Post</button>
+         <input type="file" 
+         onChange={ev=>setFiles(ev.target.files)}/>
+
+             <Editor value={content}  onChange={setContent}  />
              <button style={{marginTop: '5px'}}> Create Post</button>
          </form>
      
      );
 }
-/******  f8d59a8c-26d2-47de-91df-ce95d0dab6cb  *******/
